dd76b6c9e7cd0f40cd2a07eee867ce321b9b284e
TonytLieu <148352323+TonytLieu@users.noreply.github.com>
2023-11-30 09:06:33 -0500

    Add files via upload

---
+++ AmericaAirlines/AmericaAirlines/AmericaAirlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmericaAirlinesApp.swift
+//  AmericaAirlines
+//
+//  Created by Tony Lieu on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct AmericaAirlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
