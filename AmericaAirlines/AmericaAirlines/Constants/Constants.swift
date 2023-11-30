fb73a1e31f62b388ea3fe20c310768ba6a56445b
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-11-30 09:42:37 -0500

    Added viewModel and Constants

---
+++ AmericaAirlines/AmericaAirlines/Constants/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  AmericaAirlines
+//
+//  Created by Suguru Tokuda on 11/30/23.
+//
+
+import Foundation
+
+struct Constants {
+    static let apiEndpoint = "https://api.duckduckgo.com/"
+}
